26356a5600d27e369a81ece65e7dab68fb647ff0
Nathan Gianelli <nathan.gianelli98@gmail.com>
2020-03-12 17:38:06 -0400

    update

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const mysql = require("mysql")
 
+let connection;
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
+        port: 3306,
         user: 'root',
         password: '347*98Ng',
         database: 'krustykrab_db'
@@ -22,13 +25,13 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultlayout: "main"}));
 app.set("view engine", "handlebars");
 
-const connection = mysql.createConnection({
-    host: "localhost", 
-    port: 3306, 
-    user: "root", 
-    password: "347*98Ng", 
-    database: "krustykrab_db"
-})
+// const connection = mysql.createConnection({
+//     host: "localhost", 
+//     port: 3306, 
+//     user: "root", 
+//     password: "347*98Ng", 
+//     database: "krustykrab_db"
+// })
 
 connection.connect(function(err) {
     if (err) throw err;
